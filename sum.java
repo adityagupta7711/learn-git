702c87271ac8d73be089acef4b88a02c07dd2f3a
Aditya Gupta <guptaaditya1231@gmail.com>
2024-01-19 12:15:41 +0530

    initial commit

--- sum.java
+++ sum.java
@@ -5,6 +5,6 @@ public class sum {
 
     public static void main(String[] args) {
         int a=1; int b=2;
-        System.out.println(a+b);
+        System.out.println(" sum is" + (a+b));
     }
 }
